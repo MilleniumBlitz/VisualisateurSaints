40d80b4eb252c9489cc5fdb7eea8499b32deb348
Xavier Lafficher <xlafficher@daringit.fr>
2025-12-10 19:43:39 +0100

    Amélioration de la gestion des urls

--- app.py
+++ app.py
@@ -27,39 +27,28 @@ def query(sql, params=(), one=False):
     conn.close()
     return (result[0] if result else None) if one else result
 
-@app.route("/")
-def mois():
-
-    mois = request.args.get("mois", "01")
-
-    saints = query("SELECT id, nom, titre, jour, mois, description, source, image FROM saints WHERE mois=? ORDER BY jour", (mois,))
-
-    jours = {}
-    for saint in saints:
-        jours.setdefault(saint['jour'], []).append(saint)
-
-    return render_template("mois.html", mois=mois, saints=saints)
-
 @app.route("/<mois>/<jour>")
 def saints(mois, jour):
     saints = query("SELECT * FROM saints WHERE mois=? AND jour=? ORDER BY nom", (mois, jour))
     return render_template("saints.html", saints=saints, mois=mois, jour=jour)
 
-@app.route("/<mois>/saints")
-def saints_du_mois(mois):
+@app.route("/")
+@app.route("/<mois>")
+def saints_du_mois(mois = "01"):
     saints = query("""
         SELECT id, nom, titre, jour, date_naissance, description, source, image
         FROM saints
         WHERE mois=?
         ORDER BY CAST(jour AS INT), nom
     """, (mois,))
-    return render_template("saints_mois.html", saints=saints, mois=mois)
+    return render_template("mois.html", saints=saints)
 
 
+@app.route('/edit/', methods=["GET", "POST"])
 @app.route("/edit/<int:id>", methods=["GET", "POST"])
-def edit(id):
+def edit(id = None):
     if request.method == "POST":
-
+        
         file = request.files["image"]
 
         if file and allowed_file(file.filename):
@@ -68,19 +57,38 @@ def edit(id):
             save_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
             file.save(save_path)
 
-        query("""
-            UPDATE saints SET nom=?, titre=?, date_naissance=?, description=?, source=?, image=?
-            WHERE id=?
-        """, (
-            request.form["nom"],
-            request.form["titre"],
-            request.form["date_naissance"],
-            request.form["description"],
-            request.form["source"],
-            filename,
-            id
-        ))
-        return redirect(url_for("mois"))
+        # Si il s'agit d'un ajout
+        if id is None:
+            query("""INSERT INTO saints
+                (nom, titre, date_naissance, description, "source", jour, mois, image)
+                VALUES(?,?,?,?,?,?,?,?);""", (
+                request.form["nom"],
+                request.form["titre"],
+                request.form["date_naissance"],
+                request.form["description"],
+                request.form["source"],
+                request.form["jour"],
+                request.form["mois"],
+                filename,
+            ))
+            request.args.add("mois", request.form["mois"])
+            return redirect(url_for("mois"))
+
+        else:
+
+            query("""
+                UPDATE saints SET nom=?, titre=?, date_naissance=?, description=?, source=?, image=?
+                WHERE id=?
+            """, (
+                request.form["nom"],
+                request.form["titre"],
+                request.form["date_naissance"],
+                request.form["description"],
+                request.form["source"],
+                filename,
+                id
+            ))
+            return redirect(url_for("mois"))
 
     saint = query("SELECT * FROM saints WHERE id=?", (id,), one=True)
     return render_template("edit.html", saint=saint)
